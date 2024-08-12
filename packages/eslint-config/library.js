46fb1c042ac3693f243d23ead231b716ef7b74bc
Chris Herman <crherman7@gmail.com>
2024-08-12 15:04:18 -0400

    init repo

---
+++ packages/eslint-config/library.js
@@ -0,0 +1,43 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  plugins: ["only-warn", "import"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+    "__tests__/",
+    "jest.config.js",
+    "template/plugin",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "import/no-duplicates": ["warn", { "prefer-inline": true }],
+    "import/order": ["warn", { "newlines-between": "always" }],
+  },
+};
