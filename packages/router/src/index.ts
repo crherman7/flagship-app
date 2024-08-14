17d444efdb7da70428585395b10bba36e6ae54de
Chris Herman <crherman7@gmail.com>
2024-08-14 08:51:22 -0400

    fix: remove unused router class

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,7 +1,4 @@
 import 'react-native-url-polyfill/auto';
-import FlagshipAppRouterInstance from './FlagshipAppRouter';
 
 export * from './hooks';
-export const {shared: FlagshipAppRouter} = FlagshipAppRouterInstance;
-
 export * from './router';
