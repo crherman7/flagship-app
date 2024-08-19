23dee0aafa250ceb5dfa594d249bf23d1130644a
Chris Herman <crherman7@gmail.com>
2024-08-19 14:03:05 -0400

    feat: add screens

--- packages/env/src/components/DevMenuModal.tsx
+++ packages/env/src/components/DevMenuModal.tsx
@@ -6,20 +6,38 @@ import {
   StyleSheet,
   SafeAreaView,
 } from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
 
-import {useModal} from '../context';
+import {DevMenuContext, useModal} from '../context';
 
 const DEFAULT_MODAL_PROPS: ModalProps = {
+  /**
+   * slides in from the bottom
+   *
+   * @link {https://reactnative.dev/docs/modal#animationtype}
+   */
   animationType: 'slide',
+
+  /**
+   * covers the screen completely
+   *
+   * @link {https://reactnative.dev/docs/modal#presentationstyle-ios}
+   */
   presentationStyle: 'fullScreen',
 };
 
 export function DevMenuModal() {
-  const [visible] = useModal();
+  const [visible, setVisible] = useModal();
+
+  function onRequestClose() {
+    setVisible(false);
+  }
 
   return (
-    <Modal {...DEFAULT_MODAL_PROPS} visible={visible}>
+    <Modal
+      {...DEFAULT_MODAL_PROPS}
+      visible={visible}
+      onRequestClose={onRequestClose}>
       <SafeAreaView style={styles.safeAreaContainer}>
         <View>
           <Text>DevMenuModal</Text>
