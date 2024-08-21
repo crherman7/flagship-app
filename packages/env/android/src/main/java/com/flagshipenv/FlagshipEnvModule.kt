3e3fabe760326a260286fde0b4441f8e4f9d8e43
Chris Herman <crherman7@gmail.com>
2024-08-21 17:01:53 -0400

    fix: android env switcher

--- packages/env/android/src/main/java/com/flagshipenv/FlagshipEnvModule.kt
+++ packages/env/android/src/main/java/com/flagshipenv/FlagshipEnvModule.kt
@@ -5,6 +5,9 @@ import android.content.Context
 
 import android.content.SharedPreferences
 import android.content.pm.PackageInfo
+import android.content.res.Resources
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -12,53 +15,54 @@ import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
 class FlagshipEnvModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+    ReactContextBaseJavaModule(reactContext) {
 
-  private val sharedPref: SharedPreferences = reactContext.getSharedPreferences("__ENV_NAME_STORE", Context.MODE_PRIVATE)
+    private val resources: Resources = reactContext.applicationContext.resources
+    private val sharedPref: SharedPreferences =
+        reactContext.getSharedPreferences("__ENV_NAME_STORE", Context.MODE_PRIVATE)
 
-  override fun getName(): String {
-    return NAME
-  }
+    override fun getName(): String {
+        return NAME
+    }
 
-  override fun getConstants(): Map<String, Any> {
-    val constants = HashMap<String, Any>()
-    val context = reactApplicationContext
+    @RequiresApi(Build.VERSION_CODES.P)
+    @SuppressLint("DiscouragedApi")
+    override fun getConstants(): Map<String, Any> {
+        val constants = HashMap<String, Any>()
+        val context = reactApplicationContext
 
-    context.getString(R.string.flagship_env)
+        val envNameResourceId = resources.getIdentifier("flagship_env", "string", context.packageName)
+        val envName = resources.getString(envNameResourceId);
+        val showDevMenuResourceId = resources.getIdentifier("flagship_dev_menu", "string", context.packageName)
+        val showDevMenu = resources.getString(showDevMenuResourceId)
 
-    // TODO: get envName from strings.xml
-    val initialEnvName = "prod";
-    val envName = sharedPref.getString("envName", initialEnvName) ?: initialEnvName
+        constants["envName"] = sharedPref.getString("envName", envName) ?: envName
+        constants["showDevMenu"] = showDevMenu
+        constants["appVersion"] = getPackageInfo().versionName
+        constants["buildNumber"] = getPackageInfo().longVersionCode.toString()
 
-    constants["envName"] = envName
+        return constants
+    }
 
-    // TODO: get showDevMenu from strings.xml
-    constants["showDevMenu"] = true
-    constants["appVersion"] = getPackageInfo().versionName
-    constants["buildNumber"] = getPackageInfo().versionCode.toString()
+    @SuppressLint("ApplySharedPref")
+    @ReactMethod
+    fun setEnv(name: String, promise: Promise) {
+        val editor = sharedPref.edit()
+        editor.putString("envName", name)
+        editor.commit()
 
-    return constants
-  }
+        promise.resolve(null)
+    }
 
-  @SuppressLint("ApplySharedPref")
-  @ReactMethod
-  fun setEnv(name: String, promise: Promise) {
-      val editor = sharedPref.edit()
-      editor.putString("envName", name)
-      editor.commit()
-
-      promise.resolve(null)
-  }
-
-  @Throws(Exception::class)
-  private fun getPackageInfo(): PackageInfo {
-      val packageManager = reactApplicationContext.packageManager
-      val packageName = reactApplicationContext.packageName
-      return packageManager.getPackageInfo(packageName, 0)
-  }
+    @Throws(Exception::class)
+    private fun getPackageInfo(): PackageInfo {
+        val packageManager = reactApplicationContext.packageManager
+        val packageName = reactApplicationContext.packageName
+        return packageManager.getPackageInfo(packageName, 0)
+    }
 
 
-  companion object {
-    const val NAME = "FlagshipEnv"
-  }
+    companion object {
+        const val NAME = "FlagshipEnv"
+    }
 }
